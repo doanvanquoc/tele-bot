85fb4855a53e5b92e68819ad1e71d92f2bb64740
Đoàn Văn Quốc <134766996+doanvanquoc@users.noreply.github.com>
2025-03-01 13:14:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,10 +112,10 @@ def auto(update, context):
 
     # Tạo job mới
     job = context.job_queue.run_repeating(
-        auto_price, interval=300, first=0, context={"chat_id": chat_id, "coin": coin}
+        auto_price, interval=180, first=0, context={"chat_id": chat_id, "coin": coin}
     )
     active_jobs[job_key] = job  # Lưu job vào dictionary
-    update.message.reply_text(f"Đã set auto giá {coin} mỗi 10p, chill đi bro!")
+    update.message.reply_text(f"Đã set auto giá {coin} mỗi 3p, chill đi bro!")
 
 # Command /cancel
 def cancel(update, context):
